01f99c723295765f6771a351d7d8de243de4daea
slameryt4@gmail.com <sla1meryoutube>
2021-11-23 19:18:14 +0200

    First commit

---
+++ circles.py
@@ -0,0 +1,39 @@
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from random import randint
+
+
+class CircleWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+        uic.loadUi('UI.ui', self)
+        self.qp = QPainter()
+        self.create_circle_btn.clicked.connect(self.update)
+
+    def initUI(self):
+        self.setGeometry(300, 300, 300, 300)
+        self.setWindowTitle('Git и желтые окружности')
+
+    def paintEvent(self, event):
+        self.qp = QPainter()
+        self.qp.begin(self)
+        self.draw()
+        self.qp.end()
+
+    def draw(self):
+        self.qp.setBrush(QColor(randint(1, 255), randint(1, 100), randint(1, 100)))
+        r = randint(1, 100)
+        x = randint(0, 640)
+        y = randint(0, 480)
+        self.qp.drawEllipse(x, y, r, r)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = CircleWindow()
+    ex.show()
+    sys.exit(app.exec())
